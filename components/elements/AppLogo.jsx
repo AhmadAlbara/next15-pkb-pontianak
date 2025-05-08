c11ee12a94d7a9011f1698ee78d3b3f76e3dfe5d
peritolol <sultanml888@gmail.com>
2025-05-08 22:03:28 +0700

    Logo

--- components/elements/AppLogo.jsx
+++ components/elements/AppLogo.jsx
@@ -1,10 +1,15 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const AppLogo = () => {
   return (
-    <Link href={"/"}>
-      Logo
+    <Link href={"/"} className='flex items-center'>
+      <Image src={"/images/logo.Jpg"} width={40} height={40} />
+      <div className='ml-3'>
+        <h1 className='font-semibold text-primary '>PKB</h1>
+        <p>Pontianak</p>
+      </div>
     </Link>
   )
 }
