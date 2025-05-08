70c1d249bfdf0a0bd2ae616bffdf5575d0ca90a2
peritolol <sultanml888@gmail.com>
2025-05-08 22:24:19 +0700

    update logo

--- components/elements/AppLogo.jsx
+++ components/elements/AppLogo.jsx
@@ -1,12 +1,17 @@
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const AppLogo = () => {
   return (
-    <Link href={"/"}>
-      Logo
+    <Link href={"/"} className="flex items-center">
+      <Image src={"/images/logo.jpg"} alt="logo" width={40} height={40} />
+      <div className="ml-3 font-semibold">
+        <h1 className="text-primary">PKB</h1>
+        <p className="font-normal">Pontianak</p>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default AppLogo
+export default AppLogo;
